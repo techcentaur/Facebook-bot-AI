052e6de81a6b4186befc0cfc91dd9511826034a0
Ankit Solanki <ankit03june@gmail.com>
2018-01-11 21:19:14 +0530

    bot file: edited

--- bot.py
+++ bot.py
@@ -11,26 +11,22 @@ verify_token = 'verifythistokenotherwiseillbeatyourshitup'
 
 @bot.route('/', methods=['GET'])
 def hub_challenge_verification():
-	return "Done"
-	# if request.args.get("hub.mode")=="subscribe" and request.args.get("hub.challenge"):
-	# 	if not request.args.get("hub.verify_token") == verify_token:
-	# 		return "Verification Invalid", 403
-	# 	return request.args.get('hub.challenge','')
+	if request.args.get("hub.mode")=="subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == verify_token:
+			return "Verification Invalid", 403
+		return request.args.get('hub.challenge','')
 
 
 @bot.route('/', methods=['POST'])
 def webhook():
-	try:
-		data = json.loads(request.data)
-		messaging_events = data["entry"][0]["messaging"]
-		for event in messaging_events:
-			sender = event['sender']['id']
-			receiver = event['recipient']['id']
-			message = event['message']['text']
-			send_message(sender, "Hi")
+	data = json.loads(request.data)
+	messaging_events = data["entry"][0]["messaging"]
+	for event in messaging_events:
+		sender = event['sender']['id']
+		receiver = event['recipient']['id']
+		message = event['message']['text']
+		send_message(sender, "Hi")
 
-	except Exception as e:
-		print ("error")
 
 def send_message(person_who_will_receive, message):
 
@@ -41,7 +37,7 @@ def send_message(person_who_will_receive, message):
 	headers = {"Content-Type": "application/json"}
 	data = json.dumps({
 		"recipient":{"id": person_who_will_receive},
-		"message": {"text": message_data}
+		"message": {"text": message}
 		})
 	r=requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     # if r.status_code!=requests.codes.ok:
